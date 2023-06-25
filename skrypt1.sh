328c25161d8939b0f0f7a9d8578d5b6572a8d0f6
Krystian <krystiankaczanowski@vp.pl>
2023-06-25 17:38:36 +0200

    Wykonano zmiany na branchu taskBranch

--- skrypt1.sh
+++ skrypt1.sh
@@ -1,7 +1,7 @@
-if [[ "$1" == "--date" ]]; then
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
     current_date=$(date +%Y-%m-%d)
     echo "Dzisiejsza data: $current_date"
-elif [[ "$1" == "--logs" ]]; then
+elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
     if [[ -n "$2" ]]; then
         num_files=$2
     else
@@ -9,23 +9,47 @@ elif [[ "$1" == "--logs" ]]; then
     fi
 
     for ((i=1; i<=num_files; i++)); do
-        filename="log$i.txt"
+        filename="log$i/log$i.txt"
         echo "Nazwa pliku: $filename" >> $filename
         echo "Nazwa skryptu: $0" >> $filename
         echo "Data: $(date +%Y-%m-%d)" >> $filename
     done
 
     echo "Utworzono $num_files plików log"
-elif [[ "$1" == "--help" ]]; then
+elif [[ "$1" == "--error" || "$1" == "-e" ]]; then
+    if [[ -n "$2" ]]; then
+        num_files=$2
+    else
+        num_files=100
+    fi
+
+    for ((i=1; i<=num_files; i++)); do
+        filename="error$i/error$i.txt"
+        echo "Nazwa pliku: $filename" >> $filename
+        echo "Nazwa skryptu: $0" >> $filename
+        echo "Data: $(date +%Y-%m-%d)" >> $filename
+    done
+
+    echo "Utworzono $num_files plików error"
+elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
     echo "Dostępne opcje skryptu:"
-    echo "skrypt.sh --date : Wyświetla dzisiejszą datę."
-    echo "skrypt.sh --logs [liczba] : Tworzy automatycznie podaną liczbę plików log z informacjami o nazwie, skrypcie i dacie."
-    echo "skrypt.sh --help : Wyświetla tę pomoc."
+    echo "skrypt.sh --date (-d) : Wyświetla dzisiejszą datę."
+    echo "skrypt.sh --logs (-l) [liczba] : Tworzy automatycznie podaną liczbę plików log z informacjami o nazwie, skrypcie i dacie."
+    echo "skrypt.sh --error (-e) [liczba] : Tworzy automatycznie podaną liczbę plików error z informacjami o nazwie, skrypcie i dacie."
+    echo "skrypt.sh --help (-h) : Wyświetla tę pomoc."
+    exit 0
+elif [[ "$1" == "--init" ]]; then
+    git clone <repo_url> .
+    export PATH=$(pwd):$PATH
+    echo "Repozytorium zostało sklonowane do bieżącego katalogu."
+    echo "Ścieżka do repozytorium została dodana do zmiennej środowiskowej PATH."
+    exit 0
 else
-    echo "Nieznana opcja. Użyj skrypt.sh --help, aby uzyskać listę dostępnych opcji."
+    echo "Nieznana opcja. Użyj skrypt.sh --help (-h), aby uzyskać listę dostępnych opcji."
+    exit 1
 fi
 
-if [[ "$1" == "--date" || "$1" == "--logs" ]]; then
+if [[ "$1" == "--date" || "$1" == "--logs" || "$1" == "--error" ]]; then
     branch_name="taskBranch"
     main_branch="głównyBranch"
 
@@ -38,4 +62,3 @@ if [[ "$1" == "--date" || "$1" == "--logs" ]]; then
     git merge $branch_name
     git push origin $main_branch
 fi
-
